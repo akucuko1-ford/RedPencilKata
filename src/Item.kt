c17a13798e46d2a94b9bc57436b2ea2862eab125
Kucukoz <akucuko1@MGC12Z6498LVCG.fbpld77.ford.com>
2020-06-15 15:55:51 -0400

    Session2 completed

--- src/Item.kt
+++ src/Item.kt
@@ -1,11 +1,14 @@
 
-class Item(var price: Double) {
-
+class Item(var price: Double, daysSinceChanged: Int = 31) {
+    private var isInPromotion = false
     fun reducePrice(byPercentage: Double) {
         price -= price * byPercentage
+        isInPromotion = byPercentage >= 0.05 && byPercentage <= 0.3
     }
 
     fun isInRedPencilPromotion(): Boolean {
-        return true
+        return isInPromotion
     }
+
+
 }
--- tests/ItemTests.kt
+++ tests/ItemTests.kt
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class ItemTests {
@@ -12,15 +13,62 @@ class ItemTests {
 
     @Test
     fun reducePrice_givenAnItemWithPositivePrice_shouldReducePriceByPercentage2(){
-        val item = Item(price = 4.5)
+        val item = Item(price = 4.5, daysSinceChanged = 31)
         item.reducePrice(byPercentage = 0.2)
         assertEquals(3.6, item.price)
     }
 
     @Test
     fun isInRedPencilPromotion_givenPriceReduction_returnsTrue() {
-        val item = Item(price = 1.0)
+        val item = Item(price = 1.0, daysSinceChanged = 31)
         item.reducePrice(byPercentage = 0.2)
         assertTrue(item.isInRedPencilPromotion())
+    }
+
+    @Test
+    fun isInRedPencilPromotion_givenNoPriceReduction_returnsFalse() {
+        val item = Item(price = 1.0, daysSinceChanged = 31)
+        item.reducePrice(byPercentage = 0.0)
+        assertFalse(item.isInRedPencilPromotion())
+    }
+
+    @Test
+    fun isInRedPencilPromotion_givenPriceReductionLessThan5Percent_returnsFalse() {
+        val item = Item(price = 1.0, daysSinceChanged = 31)
+        item.reducePrice(byPercentage = 0.049)
+        assertFalse(item.isInRedPencilPromotion())
+    }
+
+    @Test
+    fun isInRedPencilReduction_givenPriceReductionIs5Percent_returnsTrue() {
+        val item = Item(price = 1.0, daysSinceChanged = 31)
+        item.reducePrice(byPercentage = 0.05)
+        assertTrue(item.isInRedPencilPromotion())
+    }
+
+    @Test
+    fun isInRedPencilPromotion_givenPriceReductionGreaterThan30Percent_returnsFalse() {
+        val item = Item(price = 1.0, daysSinceChanged = 31)
+        item.reducePrice(byPercentage = 0.31)
+        assertFalse(item.isInRedPencilPromotion())
+    }
+
+    @Test
+    fun isInRedPencilPromotion_givenPriceReductionIs30Percent_returnsTrue(){
+        val item = Item(price = 1.0, daysSinceChanged = 31)
+        item.reducePrice(byPercentage = 0.3)
+        assertTrue(item.isInRedPencilPromotion())
+    }
+
+    @Test
+    fun isInRedPencilPromotion_givenPriceDeductionIsBetween5And30Percent_returnsTrue() {
+        val item = Item(price = 1.0, daysSinceChanged = 31)
+        item.reducePrice(byPercentage = 0.13)
+        assertTrue(item.isInRedPencilPromotion())
+    }
+
+    @Test
+    fun IsPriceStable_given31DaysSinceChanged_ReturnsTrue(){
+        val item = Item(price = 1.0, daysSinceChanged = 31)
     }
 }
